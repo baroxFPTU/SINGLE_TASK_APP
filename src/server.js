70da77368abedfb50b2bd05c66084193afb1e285
Barox Phan <pqb.barox@gmail.com>
2021-10-18 16:56:31 +0700

    Fix PORT for heroku

--- src/server.js
+++ src/server.js
@@ -1,31 +1,30 @@
-import express from 'express';
-import exphbs from 'express-handlebars';
-import dotenv from 'dotenv';
+import express from "express";
+import exphbs from "express-handlebars";
+import dotenv from "dotenv";
 
-import { connectDB } from './config/database.js';
-import { initRoute } from './routes/web-app/index.js';
-import { initAPIs } from './routes/api/index.js';
+import { connectDB } from "./config/database.js";
+import { initRoute } from "./routes/web-app/index.js";
+import { initAPIs } from "./routes/api/index.js";
 
-dotenv.config({path:'src/config/.env'});
+dotenv.config({ path: "src/config/.env" });
 
-const PORT = 2703;
+const PORT = process.env.PORT || 2703;
 const app = express();
 connectDB();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 initRoute(app);
 initAPIs(app);
 
-app.use(express.static('src/public'));
+app.use(express.static("src/public"));
 
 // View engine
-app.engine('.hbs', exphbs({extname: '.hbs'}));
-app.set('view engine', '.hbs');
-app.set('views', 'src/views');
+app.engine(".hbs", exphbs({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "src/views");
 
 app.listen(PORT, () => {
-    console.log('listening on port 2703');
-})
-
+  console.log("listening on port 2703");
+});
